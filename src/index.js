1610c3ca1e90cfd9829eecb2b62c265753598fa8
Catharin <catharin@Cathie.localdomain>
2022-07-06 18:51:03 +0300

    third and fourth day of marathon without homework

--- src/index.js
+++ src/index.js
@@ -8,7 +8,19 @@ import Swiper, { Thumbs, Scrollbar } from "swiper";
 // import Swiper styles
 import "swiper/css";
 import "swiper/css/scrollbar";
+import { pagination } from "./modules/pagination";
 
+const paginationWrapper = document.querySelector(".pagination");
+
+const pageURL = new URL(location);
+const page = +pageURL.searchParams.get("page") || 1;
+
+try {
+  pagination(paginationWrapper, 12, page, 5);
+} catch (e) {
+  console.warn(e);
+  console.warn("It isnt main page");
+}
 const thumbSwiper = new Swiper(".card__slider-thumb", {
   spaceBetween: 44,
   slidesPerView: 3,
@@ -30,4 +42,12 @@ new Swiper(".card__image", {
 new Swiper(".recommended__carousel", {
   spaceBetween: 30,
   slidesPerView: 5,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
